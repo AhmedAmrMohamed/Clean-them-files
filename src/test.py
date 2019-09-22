b24e7d1e9a44c4e7c2bca98d1dd569e93c51cb59
Ahmed <ahmedbonumstelio@gmail.com>
2019-09-22 22:34:13 +0200

    docdis now is not affected by punctuiation marks

--- src/test.py
+++ src/test.py
@@ -1,11 +1,14 @@
 import engine
 import os
-path = '/media/theunderdog/cheking for bad stuff part2/Music/Shazam -  World Top 100 08.05. (2018)'
-# path = '/media/theunderdog/cheking for bad stuff part2/Music/lovely'
+# path = '/media/theunderdog/cheking for bad stuff part2/Music/Shazam -  World Top 100 08.05. (2018)'
+path = '/media/theunderdog/cheking for bad stuff part2/Musicexp/dest'
 en = engine.Engine(path)
 do = en.done
 li = set(os.listdir(path))
 for x in do:
     li.remove(x)
 
-
+fil = open('log.txt','w')
+for i in li:
+    print(i,file=fil)
+fil.close()
