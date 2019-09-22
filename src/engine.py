4c43e39d388a0918c192ea1049866068834736d6
Ahmed <ahmedbonumstelio@gmail.com>
2019-09-22 21:37:03 +0200

    engine core implemented

--- src/engine.py
+++ src/engine.py
@@ -1,33 +1,27 @@
 from docdis import Docdis
+from heap import Heap
 import os
 import math
 
 class Engine:
-    def __init__(s,src,*,dst=None,deg = 0.7):
+    def __init__(s,src,*,dst=None,degree = 0.7):
         s.distance = Docdis()
         s.src = src
-        s.done = set()
-        s.lis = []
-        s.deg = max(min(math.pi/2,s.deg),0)
-        s.tester()
+        s.deg = max(min(math.pi/2,degree),0)
+        s.done  = s.core()
 
-    def builddate(s):
-        # logger = open('log.csv','w')
-        files = os.listdir(s.src)
-        for trgt in files:
-            for comp in files:
-                if (comp,trgt) in s.done or comp == trgt:
-                    continue
-                s.done.add((comp,trgt))
-                # print(comp,trgt)
-                # print(trgt,comp)
-                res = s.distance(trgt,comp)
-                s.lis.append((trgt,comp,res))
-                # print(f'{trgt},{comp},{res}',file = logger)
-        logger.close()
-        return s.lis
-        
+    def core(s):
+        done = set()
+        for trgt in os.listdir(s.src):
+           add = True
+           for comp in done:
+               dis = s.distance(trgt,comp)
+               if dis < s.deg:
+                   add = False
+                   break
+           if add:
+               done.add(trgt)
+        return done
 
-
-
+                
 
--- src/test.py
+++ src/test.py
@@ -1,12 +1,11 @@
 import engine
+import os
 path = '/media/theunderdog/cheking for bad stuff part2/Music/Shazam -  World Top 100 08.05. (2018)'
 # path = '/media/theunderdog/cheking for bad stuff part2/Music/lovely'
 en = engine.Engine(path)
-li = sorted(en.lis,key = lambda x:x[2])
-for i in li:
-    if i[2] < 1:
-        print(i)
-# from docdis import Docdis
-# dis = Docdis()
-# x=dis('001 Ahmed Amr', '002 Ahmed Amr Mohamed')
-# print(x)
+do = en.done
+li = set(os.listdir(path))
+for x in do:
+    li.remove(x)
+
+
