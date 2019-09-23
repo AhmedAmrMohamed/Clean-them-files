c407a87dd8e4f546ef2a4b6e7b9ff88e044cf1ea
Ahmed <ahmedbonumstelio@gmail.com>
2019-09-23 23:49:27 +0200

    implemented copier method in engine

--- src/engine.py
+++ src/engine.py
@@ -1,16 +1,22 @@
 from docdis import Docdis
-from heap import Heap
 import os
+import shutil
 import math
 
 class Engine:
     def __init__(s,src,*,dst=None,degree = 0.7):
         s.distance = Docdis()
         s.src = src
+        s.dst = dst
         s.deg = max(min(math.pi/2,degree),0)
         s.done  = s.core()
+        s.copier()
 
     def core(s):
+        '''
+        return a set of the file names where every pairwise
+        elemets have a distance > degree
+        '''
         done = set()
         for trgt in os.listdir(s.src):
            add = True
@@ -23,5 +29,19 @@ class Engine:
                done.add(trgt)
         return done
 
-                
+    def setupdst(s):
+        dst = s.dst
+        if '~' in dst:
+            dst = os.path.exapnduser(dst)
+        dst = dst.replace('\\' , '/')
+        if not os.path.isdir(dst):
+            os.mkdir(dst)
+
+    def copier(s):
+        s.setupdst()
+        cp = shutil.copy
+        for sng in s.done:
+            print(f'cp {sng}')
+            cp(s.src+'/'+sng,s.dst+'/'+sng)
+            
 
